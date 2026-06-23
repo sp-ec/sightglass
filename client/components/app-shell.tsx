174b7342ca88f87faa119289f1d493005a0499e1
Spectrobyte <48649161+Spectromeda@users.noreply.github.com>
2026-06-23 12:33:00 -0400

    updated app shell to include page title

--- client/components/app-shell.tsx
+++ client/components/app-shell.tsx
@@ -6,11 +6,11 @@ import { usePathname } from "next/navigation";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
 import { AppSidebar } from "./sidebar/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "./ui/sidebar";
+import { Separator } from "@/components/ui/separator";
 
 const navigation = [
 	{ href: "/queries", label: "Queries" },
@@ -34,36 +34,27 @@ function ThemeToggle() {
 	);
 }
 
+const routeTitles: Record<string, string> = {
+	"/sync": "Sync Steam Data",
+	"/browse": "Browse Games",
+};
+
 export function AppShell({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
 
 	return (
 		<div className="min-h-screen bg-background text-foreground">
-			{/* <header className="border-b bg-card/20 flex">
-        <div className="flex h-16 w-full items-center justify-between px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center gap-6">
-            <Link href="/queries" className="text-lg font-semibold tracking-tight">
-              Steam Analyzer
-            </Link>
-            <nav className="flex items-center gap-2">
-              {navigation.map((item) => {
-                const active = pathname === item.href;
-
-                return (
-                  <Button key={item.href} asChild variant={active ? "default" : "ghost"} size="lg">
-                    <Link href={item.href}>{item.label}</Link>
-                  </Button>
-                );
-              })}
-            </nav>
-          </div>
-          <ThemeToggle />
-        </div>
-      </header> */}
-			<SidebarProvider >
+			<SidebarProvider>
 				<AppSidebar />
 				<main className="mx-auto w-full px-4 py-6 sm:px-6 lg:px-8">
-					<SidebarTrigger />
+					<header className="flex items-center gap-4">
+						<SidebarTrigger className="cursor-pointer" />
+						<Separator orientation="vertical" />
+						<h1 className="text-sm font-semibold">
+							{routeTitles[pathname] || "Page Title"}
+						</h1>
+					</header>
+
 					{children}
 				</main>
 			</SidebarProvider>
